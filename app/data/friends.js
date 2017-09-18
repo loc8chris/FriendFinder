68d6599c95a82bb3513a1a39e1d98e6ca8576d63
loc8chris <loc8chris@gmail.com>
2017-09-17 23:02:58 -0500

    Created friends.jsfile

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,56 @@
-let friends = [ {name: "Kim", imageurl: "https://pbs.twimg.com/profile_images/875432723837362176/j5NOs7Dj_400x400.jpg", questions: [1,1,1,1,1,1,1,1,1,1]},
+
+//numbers are hard coded in simple combinations for testability
+let friends = [
+
+    {name: "Kim", imageurl: "https://pbs.twimg.com/profile_images/875432723837362176/j5NOs7Dj_400x400.jpg", questions: [1,1,1,1,1,1,1,1,1,1]},
     {name: "Joe", imageurl: "https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/review_gallery_carousel_slide_thumbnail/public/screenshots/csm-movie/joedirtscreenshot2.jpg?itok=-Id5UPBV", questions: [2,2,2,2,2,2,2,2,2,2]},
     {name: "Mary", imageurl: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-theres-something-about-mary-cameron-diaz.jpg", questions: [3,3,3,3,3,3,3,3,3,3]},
     {name: "Dawn", imageurl: "http://i.dailymail.co.uk/i/pix/2015/10/26/01/1E23043000000578-3288643-Dawn_French_has_revealed_that_she_could_no_longer_live_with_her_-a-4_1445822339907.jpg", questions: [4,4,4,4,4,4,4,4,4,4]},
     {name: "Robert", imageurl: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAwNDU3MzcyOV5BMl5BanBnXkFtZTcwMjc0MTIxMw@@._V1_UY317_CR13,0,214,317_AL_.jpg", questions: [5,5,5,5,5,5,5,5,5,5]},
-    {name: "Susan", imageurl: "https://pbs.twimg.com/profile_images/686633235393163266/cq_ikTmA.jpg", questions: [1,2,1,2,1,2,1,2,1,2]}];
+    {name: "Susan", imageurl: "https://pbs.twimg.com/profile_images/686633235393163266/cq_ikTmA.jpg", questions: [1,2,1,2,1,2,1,2,1,2]},
+]
 
-function getFriends(){
+function getFriends(){    
     return friends;
+}
 
-}
 function getBestMatch(friend){
-    return friends[0];
+    let i = 0;
+
+    let lowest = 50;
+    let friendIndex = -1;
+    let j = 0;
+    
+    //TODO: algorithm for friends
+    for(i =0;i<friends.length;i++){
+        
+        //ignore any friends with the same name
+        //it could be the same person
+        if(friend.name == friends[i].name) continue;
+
+        let score = 0;
+
+        for(j=0;j<friend.questions.length; j++){
+            score += Math.abs(friends[i].questions[j] - friend.questions[j]);
+        }
+
+        if(score < lowest){
+            friendIndex = i;
+            lowest = score;
+        }
+    }
+
+    return friends[friendIndex];
 }
+
 function setNewFriend(friend){
-    friends.push(friend)
+    //save friend to file system
+    friends.push(friend);
 }
+
+
 module.exports = {
+
     getBestMatch: getBestMatch,
     setNewFriend: setNewFriend,
     getFriends: getFriends
